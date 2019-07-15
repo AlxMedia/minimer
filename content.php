2dd6a7199f813080ab7e1aecc6ca6c21f6487a5d
Alexander Agnarson <alexander.agnarson@gmail.com>
2019-07-15 23:39:08 +0200

    1.1.1

--- content.php
+++ content.php
@@ -20,10 +20,12 @@
 			<?php else: ?>
 				<?php if ( has_post_thumbnail() ) : ?>
 					<a href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail('minimer-large'); ?>
-						<?php if ( has_post_format('video') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-play"></i></span>'; ?>
-						<?php if ( has_post_format('audio') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-volume-up"></i></span>'; ?>
-						<?php if ( is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-star"></i></span>'; ?>
+						<div class="image-container">
+							<?php the_post_thumbnail('minimer-large'); ?>
+							<?php if ( has_post_format('video') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-play"></i></span>'; ?>
+							<?php if ( has_post_format('audio') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-volume-up"></i></span>'; ?>
+							<?php if ( is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-star"></i></span>'; ?>
+						</div>
 					</a>
 				<?php endif; ?>
 			<?php endif; ?>
