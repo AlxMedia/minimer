329469601f3014a83773f9992b14126d473b88ab
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-03-03 12:38:47 +0100

    1.2.9

--- footer.php
+++ footer.php
@@ -46,7 +46,7 @@
 		
 		<?php if ( has_nav_menu('footer') ): ?>
 			<div id="wrap-nav-footer" class="wrap-nav">
-				<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'footer','menu_id' => 'nav-footer','fallback_cb'=> false)); ?>
+				<?php \Minimer\Nav::nav_menu(array('theme_location'=>'footer','menu_id' => 'nav-footer','fallback_cb'=> false)); ?>
 			</div>
 		<?php endif; ?>
 		
--- functions.php
+++ functions.php
@@ -90,15 +90,17 @@ add_action( 'after_setup_theme', 'minimer_setup' );
 
 /*  Custom navigation
 /* ------------------------------------ */
-require_once 'functions/nav.php';
-$nav = new \AlxMedia\Nav();
-$nav->enqueue(
-	[
-		'script' => 'js/nav.js',
-		'inline' => false,
-	]
-);
-$nav->init();
+add_action( 'wp', function() {
+	require_once 'functions/nav.php';
+	$nav = new \Minimer\Nav();
+	$nav->enqueue(
+		[
+			'script' => 'js/nav.js',
+			'inline' => false,
+		]
+	);
+	$nav->init();
+} );
 
 
 /*  Custom logo
