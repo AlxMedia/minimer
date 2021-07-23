c5995161f55b18cf725576d2c988109395a28f13
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:46:12 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -66,6 +66,9 @@ if ( ! function_exists( 'minimer_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'minimer-small', 200, 200, true );
 		add_image_size( 'minimer-medium', 520, 292, true );
